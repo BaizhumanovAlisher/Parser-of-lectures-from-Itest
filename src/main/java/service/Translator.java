e610bc091a9c45d9457f5535e6ddbedee7968405
Baizhumanov Alisher <baizhumanovalisher07@mail.ru>
2023-07-15 10:11:40 +0600

    refactor

--- src/main/java/service/Translator.java
+++ src/main/java/service/Translator.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import model.TranslatedText;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.HttpURLConnection;
@@ -16,6 +17,28 @@ public class Translator {
         this.mapper = mapper;
     }
 
+
+
+    public String translate(String text) {
+        HttpURLConnection connection = null;
+        try {
+            String body = mapper.writeValueAsString(new TranslatedText(text));
+
+            connection = initializeConnection();
+            requestToServer(connection, body);
+            checkConnectionCode(connection);
+            String respond = getString(connection);
+
+            return mapper.readValue(respond, TranslatedText.class).toString();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        } finally {
+            if (connection != null) {
+                connection.disconnect();
+            }
+        }
+    }
+
     private HttpURLConnection initializeConnection() throws Exception {
         URL url = new URL("https://libretranslate.com/translate");
         HttpURLConnection connection = (HttpURLConnection) url.openConnection();
@@ -25,31 +48,20 @@ public class Translator {
         return connection;
     }
 
-    public String translate(String text) {
-        HttpURLConnection connection = null;
-        try {
-            connection = initializeConnection();
+    private String getString(HttpURLConnection connection) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader((connection.getInputStream())));
+        return br.readLine();
+    }
 
-            String body = mapper.writeValueAsString(new TranslatedText(text));
-
-            OutputStream outputStream = connection.getOutputStream();
-            outputStream.write(body.getBytes());
-            outputStream.flush();
-
-            if (connection.getResponseCode() != HttpURLConnection.HTTP_OK) {
-                throw new RuntimeException("Failed : HTTP error code : " + connection.getResponseCode());
-            }
-
-            BufferedReader br = new BufferedReader(new InputStreamReader((connection.getInputStream())));
-            String line = br.readLine();
-
-            return mapper.readValue(line, TranslatedText.class).toString();
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        } finally {
-            if (connection != null) {
-                connection.disconnect();
-            }
+    private void checkConnectionCode(HttpURLConnection connection) throws IOException {
+        if (connection.getResponseCode() != HttpURLConnection.HTTP_OK) {
+            throw new RuntimeException("Failed : HTTP error code : " + connection.getResponseCode());
         }
+    }
+
+    private void requestToServer(HttpURLConnection connection, String body) throws IOException {
+        OutputStream outputStream = connection.getOutputStream();
+        outputStream.write(body.getBytes());
+        outputStream.flush();
     }
 }
