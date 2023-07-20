c213e0d42a01736eb4605d1f1ef6c3c748bfa16b
Baizhumanov Alisher <baizhumanovalisher07@mail.ru>
2023-07-21 02:43:23 +0600

    update main

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,24 +1,32 @@
 import builder.WebParserBuilder;
 import model.Chapter;
 import model.Lecture;
+import model.LectureLink;
 import service.FileSystem;
 import service.WebParser;
 
+import java.io.IOException;
 import java.util.List;
 
 public class Application {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         String domainName = "https://itest.kz";
 
         WebParser webParser = WebParserBuilder.build();
-        FileSystem fileSystem = new FileSystem("/home/alisher/Desktop");
+        FileSystem fileSystem = new FileSystem("/home/alisher/Desktop/History of Kazakhstan", "lecture", "audio");
 
+        fileSystem.startWriteOutline();
         List<Chapter> chapters = webParser.getChapters(domainName, "/ru/attestation/istoriya-kazahstana-4077");
-        fileSystem.makeParentFolder("History of Kazakhstan");
 
         for (Chapter chapter : chapters) {
-            List<Lecture> lectures = webParser.getLectures(domainName, chapter);
-            fileSystem.makeChildFolder(chapter.title());
+            fileSystem.writeChapterNameInOutline();
+
+            for (LectureLink lectureLink : chapter.lectureLinks()) {
+                Lecture lecture = webParser.getLecture(lectureLink.id(), domainName, lectureLink.url());
+                fileSystem.saveLecture(lecture);
+            }
         }
+
+        fileSystem.endWriteOutline();
     }
 }
