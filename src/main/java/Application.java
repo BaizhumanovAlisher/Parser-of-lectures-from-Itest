0c1f9e15bd6224813928b2de002fb069a13c6e90
Baizhumanov Alisher <baizhumanovalisher07@mail.ru>
2023-07-22 01:59:50 +0600

    complete project

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -5,28 +5,34 @@ import model.LectureLink;
 import service.FileSystem;
 import service.WebParser;
 
-import java.io.IOException;
 import java.util.List;
 
 public class Application {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         String domainName = "https://itest.kz";
 
         WebParser webParser = WebParserBuilder.build();
         FileSystem fileSystem = new FileSystem("/home/alisher/Desktop/History of Kazakhstan", "lecture", "audio");
 
-        fileSystem.startWriteOutline();
         List<Chapter> chapters = webParser.getChapters(domainName, "/ru/attestation/istoriya-kazahstana-4077");
 
+        fileSystem.startWriting();
+        fileSystem.writeText("# OUTLINE\n\n\n\n");
+
         for (Chapter chapter : chapters) {
-            fileSystem.writeChapterNameInOutline();
+            fileSystem.writeText("### " + chapter.title() + "\n\n");
 
             for (LectureLink lectureLink : chapter.lectureLinks()) {
                 Lecture lecture = webParser.getLecture(lectureLink.id(), domainName, lectureLink.url());
-                fileSystem.saveLecture(lecture);
+
+                fileSystem.saveAudio(lecture.transliteratedText() + ".mp3", lecture.audio());
+                fileSystem.saveLecture(lecture.title(), lecture.transliteratedText() + ".md", lecture.lecture());
+                fileSystem.writeText(String.format("[%s](%s.md)\n\n", lecture.title(), lecture.transliteratedText()));
             }
+
+            fileSystem.writeText("\n\n");
         }
 
-        fileSystem.endWriteOutline();
+        fileSystem.endWriting();
     }
 }
